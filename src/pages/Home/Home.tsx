f8b1e95b8b142bae8f393ef94ebc2051726e01f4
Vitaly Yurkin <jvn9405@gmail.com>
2024-09-10 22:01:02 +0300

    Обработка загрузки

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,16 +4,26 @@ import ProductCard from '../../components/ProductCard/ProductCard';
 import { PREFIX } from '../../helpers/API';
 import { Product } from '../../interfaces/product.interface';
 import axios from 'axios';
+import Loader from '../../components/Loader/Loader';
 
 export function Home() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const getProducts = async () => {
         try {
+            setIsLoading(true);
+            await new Promise<void>(resolve => {
+                setTimeout(() => {
+                    resolve();
+                }, 2000);
+            });
             const { data } = await axios.get<Product[]>(`${PREFIX}/plants`);
             setProducts(data);
+            setIsLoading(false);
         } catch (e) {
             console.error(e);
+            setIsLoading(false);
             return;
         }
 
@@ -44,16 +54,18 @@ export function Home() {
             </Headling>
             <Headling classLevel={3}>Barberton Daisy</Headling>
             <div>
-                {products.map(p => (
-                    <ProductCard
-                        key={p.id}
-                        id={p.id}
-                        name={p.name}
-                        price={p.price}
-                        oldPrice={p.oldPrice}
-                        image={p.image}
-                    />
-                ))}
+                {!isLoading &&
+                    products.map(p => (
+                        <ProductCard
+                            key={p.id}
+                            id={p.id}
+                            name={p.name}
+                            price={p.price}
+                            oldPrice={p.oldPrice}
+                            image={p.image}
+                        />
+                    ))}
+                {isLoading && <Loader />}
             </div>
         </>
     );
