a3fbb109604803d0e6f38256f6c4ffad46ecf8f7
Vitaly Yurkin <jvn9405@gmail.com>
2024-09-13 16:48:52 +0300

    Работающая панинация

--- src/main.tsx
+++ src/main.tsx
@@ -64,7 +64,7 @@ const router = createBrowserRouter([
     },
     {
         path: '*',
-        element: <Error />
+        element: <ErrorPage />
     }
 ]);
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,11 +5,16 @@ import { Product } from '../../interfaces/product.interface';
 import axios, { AxiosError } from 'axios';
 import Loader from '../../components/Loader/Loader';
 import { PlantsList } from './PlantsList/PlantsList';
+import styles from './Home.module.css';
+import cn from 'classnames';
 
 export function Home() {
     const [products, setProducts] = useState<Product[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | undefined>();
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const productsPerPage = 9;
+    const visiblePaginationButtons = 3;
 
     const getProducts = async () => {
         try {
@@ -31,6 +36,80 @@ export function Home() {
         getProducts();
     }, []);
 
+    // Calculate the current products to display
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = products.slice(
+        indexOfFirstProduct,
+        indexOfLastProduct
+    );
+
+    // Calculate total pages
+    const totalPages = Math.ceil(products.length / productsPerPage);
+
+    // Pagination handlers
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+    };
+
+    const renderPagination = () => {
+        const paginationButtons = [];
+        for (let i = 1; i <= totalPages; i++) {
+            if (i === currentPage) {
+                paginationButtons.push(
+                    <button className={styles['active']} key={i}>
+                        {i}
+                    </button>
+                );
+            } else {
+                paginationButtons.push(
+                    <button
+                        className={cn({
+                            [styles['invisible']]:
+                                (i > visiblePaginationButtons &&
+                                    currentPage != totalPages) ||
+                                (i < visiblePaginationButtons &&
+                                    currentPage > visiblePaginationButtons)
+                        })}
+                        key={i}
+                        onClick={() => handlePageChange(i)}
+                    >
+                        {i}
+                    </button>
+                );
+            }
+        }
+
+        // Add next page button
+        if (
+            currentPage <= visiblePaginationButtons ||
+            (currentPage > visiblePaginationButtons && currentPage < totalPages)
+        ) {
+            paginationButtons.push(
+                <button
+                    key="next"
+                    onClick={() => handlePageChange(currentPage + 1)}
+                >
+                    {' > '}
+                </button>
+            );
+        }
+
+        // Add previous page button
+        if (currentPage > visiblePaginationButtons) {
+            paginationButtons.unshift(
+                <button
+                    key="prev"
+                    onClick={() => handlePageChange(currentPage - 1)}
+                >
+                    {' < '}
+                </button>
+            );
+        }
+
+        return paginationButtons;
+    };
+
     return (
         <>
             <Headling>
@@ -42,8 +121,9 @@ export function Home() {
             <Headling classLevel={3}>Barberton Daisy</Headling>
             <div>
                 {error && <>{error}</>}
-                {!isLoading && <PlantsList products={products} />}
+                {!isLoading && <PlantsList products={currentProducts} />}
                 {isLoading && <Loader />}
+                <div>{renderPagination()}</div>
             </div>
         </>
     );
