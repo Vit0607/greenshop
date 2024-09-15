54e1e57e2f5114aee286c1af3941f6de377d9752
Vitaly Yurkin <jvn9405@gmail.com>
2024-09-15 15:04:38 +0300

    AppRouter.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,75 +1,11 @@
-import { lazy, StrictMode, Suspense } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Cart } from './pages/Cart/Cart.tsx';
-import { Error as ErrorPage } from './pages/Error/Error.tsx';
-import { Header } from './layout/Header/Header.tsx';
-import { PlantCare } from './pages/PlantCare/PlantCare.tsx';
-import { Shop } from './pages/Shop/Shop.tsx';
-import { Blogs } from './pages/Blogs/Blogs.tsx';
-import { Product } from './pages/Product/Product.tsx';
-import axios from 'axios';
-import { PREFIX } from './helpers/API.ts';
-import Loader from './components/Loader/Loader.tsx';
 
-const Home = lazy(() => import('./pages/Home/Home.tsx'));
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Header />,
-        children: [
-            {
-                path: '/',
-                element: (
-                    <Suspense fallback={<Loader />}>
-                        <Home />
-                    </Suspense>
-                )
-            },
-            {
-                path: '/cart',
-                element: <Cart />
-            },
-            {
-                path: '/shop',
-                element: <Shop />
-            },
-            {
-                path: '/care',
-                element: <PlantCare />
-            },
-            {
-                path: '/blogs',
-                element: <Blogs />
-            },
-            {
-                path: '/product/:id',
-                element: <Product />,
-                errorElement: <>Ошибка</>,
-                loader: async ({ params }) => {
-                    await new Promise<void>(resolve => {
-                        setTimeout(() => {
-                            resolve();
-                        }, 2000);
-                    });
-                    const { data } = await axios.get(
-                        `${PREFIX}/plantts/${params.id}`
-                    );
-                    return data;
-                }
-            }
-        ]
-    },
-    {
-        path: '*',
-        element: <ErrorPage />
-    }
-]);
+import AppRouter from './AppRouter.tsx';
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <RouterProvider router={router} />
-    </StrictMode>
+  <StrictMode>
+    <AppRouter />
+  </StrictMode>
 );
