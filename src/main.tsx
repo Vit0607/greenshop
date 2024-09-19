0a83c75e7d2980df6525524e4b052b883b4e92f3
Vitaly Yurkin <jvn9405@gmail.com>
2024-09-19 11:30:34 +0300

    Подключение reset.css и normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './styles/reset.css';
+import './styles/normalize.css';
 import './index.css';
 
 import AppRouter from './AppRouter.tsx';
